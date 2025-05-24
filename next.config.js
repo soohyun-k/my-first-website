41b7ec12223dc7458cde55b4f68ce00819b413cb
고수현 <kohsoohyun@suhyeon-ui-MacBook-Pro.local>
2025-05-24 11:40:13 +0900

    Fix assetPrefix configuration

--- next.config.js
+++ next.config.js
@@ -3,7 +3,10 @@ const nextConfig = {
   output: 'export',
   images: {
     unoptimized: true
-  }
+  },
+  assetPrefix: '/',
+  trailingSlash: true,
+  basePath: ''
 }
 
 module.exports = nextConfig
